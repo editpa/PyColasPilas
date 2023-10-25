57ccec941d50d47787bde171d9bc72b5755150b0
editpa <editpa@localhost.localdomain>
2023-10-25 09:25:38 -0600

    Código

---
+++ actividad23.py
@@ -0,0 +1,42 @@
+
+
+
+class Cola:
+    def __init__(self):
+        self.items = []
+
+    def cola(self,x):
+        self.items.append(x)
+    
+     def colaF(self):
+        try:
+            return self.items.pop(0)
+        except:
+            raise ValueError("La cola está vacía")
+    def print_cola(self):
+        print(self.items)
+
+
+class Pila:
+    def _init_(self):
+        self.items = []
+    
+     
+    def push(self, item): 
+        self.items.insert(0, item)
+     
+    def pop(self): 
+        return self.items.pop(0)
+     
+    def print_pila(self):
+        print(self.items)
+
+
+#Ejemplo de uso 
+Mpila = Pila()
+Mpila.push("Eduardo")
+Mpila.print_pila()
+
+Mcola = Cola()
+Mcola.cola("José")
+Mcola.print_cola()
