7eb2f57aaaa07852b448567e194be10c4e2e4ed1
JUANPRIROM <L221050159@pabellon.tecnm.mx>
2023-10-25 19:04:01 -0600

    commit Juan

--- actividad23_Juan.py
+++ actividad23_Juan.py
@@ -40,3 +40,21 @@ Mpila.print_pila()
 Mcola = Cola()
 Mcola.cola("José")
 Mcola.print_cola()
+
+
+
+Lista1 = [i for i in range(1, 11)]
+Lista2 = [i for i in range(1, 11)]
+
+
+def Imprimir_lista(Lista):
+    for elemento in Lista:
+        print(elemento, end=' ')
+    print()
+
+
+print("Contenido de la lista 1:")
+Imprimir_lista(Lista1)
+
+print("Contenido de la lista 2:")
+Imprimir_lista(Lista2)
