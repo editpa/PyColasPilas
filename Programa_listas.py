81044ec7333d5d4bcc020d79f903191457a72a02
Maria Fernanda Pantoja Castillo <L221050160@pabellon.tecnm.mx>
2023-10-30 09:54:34 -0600

    fer

---
+++ Programa_listas.py
@@ -0,0 +1,14 @@
+lista1=[]
+lista2=[]
+def llenar_lista1():
+    for i in range(1, 11):
+        lista1.append(i)
+
+def llenar_lista2():
+    for i in range(1, 11):
+        lista2.append(i)
+llenar_lista1()
+llenar_lista2()
+
+print(lista1)
+print(lista2)
