3dce468bc50ce796f91847df1e6d10fe0dcebf8c
unknown <martinezsantosaylin6@gmail.com>
2023-10-30 07:51:52 -0600

    aylin

---
+++ lista.py
@@ -0,0 +1,14 @@
+lista1=[]
+lista2=[]
+def llenar_lista1():
+    for i in range(1, 11):
+        lista1.append(1)
+        
+def llenar_lista2():
+    for i in range(1, 11):
+        lista2.append(1)
+llenar_lista1()
+llenar_lista2()
+
+print(lista1)
+print(lista2)
